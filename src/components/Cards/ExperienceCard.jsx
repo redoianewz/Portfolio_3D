eac3ebd56b263bd45e332a79e69201046f544ed0
redoiane ouaaziz <ouaazizredouane978@gmail.com>
2024-02-01 21:47:11 +0100

    update know 1s

--- src/components/Cards/ExperienceCard.jsx
+++ src/components/Cards/ExperienceCard.jsx
@@ -146,40 +146,41 @@ const Skill = styled.div`
 
 const ExperienceCard = ({ experience }) => {
     return (
-      <Card>
-        <Top>
-          <Image src={experience.img} />
-          <Body>
-            <Role>{experience.role}</Role>
-            <Company>{experience.company}</Company>
-            <Date>{experience.date}</Date>
-          </Body>
-        </Top>
-        <Description>
-          {experience?.desc && <Span>{experience?.desc}</Span>}
-          {experience?.skills && (
-            <>
-              <br />
-              <Skills>
-                <b>Skills:</b>
-                <ItemWrapper>
-                  {experience?.skills?.map((skill, index) => (
-                    <Skill>• {skill}</Skill>
-                  ))}
-                </ItemWrapper>
-              </Skills>
-            </>
-          )}
-        </Description>
-        {experience.doc && (
-          <a href={experience.doc} target="new">
-            <Document
-              src={experience.doc}            
-            />
-          </a>
-        )}
-      </Card>
-    );
+        <Card>
+            <Top>
+                <Image src={experience.img} />
+                <Body>
+                    <Role>{experience.role}</Role>
+                    <Company>{experience.company}</Company>
+                    <Date>{experience.date}</Date>
+                </Body>
+            </Top>
+            <Description>
+                {experience?.desc &&
+                    <Span>{experience?.desc}</Span>
+
+                }
+                {experience?.skills &&
+                    <>
+                        <br />
+                        <Skills>
+                            <b>Skills:</b>
+                            <ItemWrapper>
+                                {experience?.skills?.map((skill, index) => (
+                                    <Skill>• {skill}</Skill>
+                                ))}
+                            </ItemWrapper>
+                        </Skills>
+                    </>
+                }
+            </Description>
+            {experience.doc &&
+                <a href={experience.doc} target="new">
+                    <Document src={experience.doc} />
+                </a>
+            }
+        </Card>
+    )
 }
 
 export default ExperienceCard
